8be3bd1983798df38a14e70028f2687e40c0d426
Mark N. Beebe <mark.beebe@syccuxas01.pcc.edu>
2020-02-22 14:54:47 -0800

    Add initial Car class header and implement files

---
+++ Car.cpp
@@ -0,0 +1,11 @@
+/*
+*	Car.cpp
+*	Implements Car Class
+* 	Description: Represents a car, make, model, odometer, etc.
+*	
+*	Mark N. Beebe
+*	2/22/2020
+*/
+
+
+
---
+++ Car.h
@@ -0,0 +1,35 @@
+/*
+*	Car.h
+*	Specifies Car Class
+* 	Description: Represents a car, make, model, odometer, etc.
+*	
+*	Mark N. Beebe
+*	2/22/2020
+*/
+#ifndef _CAR_
+#define _CAR_
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Car {
+
+public:
+
+private:
+	string make;
+	string model;
+	string color;
+	int year;
+	int odometer;
+	
+	
+};
+
+
+
+
+
+#endif // _CAR_
